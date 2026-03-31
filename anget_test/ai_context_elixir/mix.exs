f900cef44240c9de1f4bb5324f35fc11f7695e45
Anderson Faustino Lima <andersonfaustinolima@gmail.com>
2026-03-31 02:15:09 -0300

    test(context): add active blueprint CRUD matrix cases across python js and elixir

---
+++ anget_test/ai_context_elixir/mix.exs
@@ -0,0 +1,12 @@
+defmodule AiContextElixir.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ai_context_elixir,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      deps: []
+    ]
+  end
+end
